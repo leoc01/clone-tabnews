1bd80772c9bb90296d7172611f706a957b7a62f4
leoc01 <leonardocampetti@gmail.com>
2024-11-16 22:02:52 -0300

    finish `api/v1/status` and test

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,9 +1,29 @@
 import database from "infra/database.js";
 
 async function status(request, response) {
-  const result = await database.query("SELECT 1 + 1 as sum;");
-  console.log(result.rows);
-  response.status(200).json({ chave: "são legais" });
+  const updatedAt = new Date().toISOString();
+  const maxConnectionsResult = await database.query("SHOW MAX_CONNECTIONS;");
+  const maxConnections = parseInt(
+    maxConnectionsResult.rows[0]?.max_connections,
+  );
+  const databaseName = process.env.POSTGRES_DB;
+  const openedConnectionsResult = await database.query({
+    text: "SELECT count(*)::int FROM pg_stat_activity WHERE datname = $1;",
+    values: [databaseName],
+  });
+  const openedConnections = openedConnectionsResult.rows[0]?.count;
+  const versionResult = await database.query("SHOW server_version;");
+  const version = versionResult.rows[0]?.server_version;
+  response.status(200).json({
+    updated_at: updatedAt,
+    dependencies: {
+      database: {
+        max_connections: maxConnections,
+        opened_connections: openedConnections,
+        version: version,
+      },
+    },
+  });
 }
 
 export default status;
--- tests/integration/api/v1/status/get.test.js
+++ tests/integration/api/v1/status/get.test.js
@@ -1,4 +1,13 @@
 test("GET to /api/v1/status should return 200", async () => {
   const response = await fetch("http://localhost:3000/api/v1/status");
   expect(response.status).toBe(200);
+
+  const responseBody = await response.json();
+
+  expect(responseBody.dependencies.database.max_connections).toBe(100);
+  expect(responseBody.dependencies.database.opened_connections).toBe(1);
+  expect(responseBody.dependencies.database.version).toBe("16.4");
+
+  const parsedUpdatedAt = new Date(responseBody.updated_at).toISOString();
+  expect(responseBody.updated_at).toEqual(parsedUpdatedAt);
 });
