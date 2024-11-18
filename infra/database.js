4bbffb85f9f2ff336016b86282a0ff3755b0194c
leoc01 <leonardocampetti@gmail.com>
2024-11-18 11:01:51 -0300

    fix `database.js`

--- infra/database.js
+++ infra/database.js
@@ -2,14 +2,15 @@ import { Client } from "pg";
 
 async function query(queryObject) {
   const client = new Client({
-    hots: process.env.POSTGRES_HOST,
+    host: process.env.POSTGRES_HOST,
     port: process.env.POSTGRES_PORT,
     user: process.env.POSTGRES_USER,
     database: process.env.POSTGRES_DB,
     password: process.env.POSTGRES_PASSWORD,
+    //ssl: true,
   });
   console.log("Credenciais do postgres:", {
-    hots: process.env.POSTGRES_HOST,
+    host: process.env.POSTGRES_HOST,
     port: process.env.POSTGRES_PORT,
     user: process.env.POSTGRES_USER,
     database: process.env.POSTGRES_DB,
--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -3,17 +3,15 @@ import database from "infra/database.js";
 async function status(request, response) {
   const updatedAt = new Date().toISOString();
   const maxConnectionsResult = await database.query("SHOW MAX_CONNECTIONS;");
-  const maxConnections = parseInt(
-    maxConnectionsResult.rows[0]?.max_connections,
-  );
+  const maxConnections = parseInt(maxConnectionsResult.rows[0].max_connections);
   const databaseName = process.env.POSTGRES_DB;
   const openedConnectionsResult = await database.query({
     text: "SELECT count(*)::int FROM pg_stat_activity WHERE datname = $1;",
     values: [databaseName],
   });
-  const openedConnections = openedConnectionsResult.rows[0]?.count;
+  const openedConnections = openedConnectionsResult.rows[0].count;
   const versionResult = await database.query("SHOW server_version;");
-  const version = versionResult.rows[0]?.server_version;
+  const version = versionResult.rows[0].server_version;
   response.status(200).json({
     updated_at: updatedAt,
     dependencies: {
