4fd7574c070c670e34624529d1d17e27f0c8a894
leoc01 <leonardocampetti@gmail.com>
2024-11-19 12:23:18 -0300

    adds `/api/v1/migrations` endpoint

---
+++ tests/integration/api/v1/migrations/post.test.js
@@ -0,0 +1,30 @@
+import database from "infra/database.js";
+
+beforeAll(cleanDatabase);
+async function cleanDatabase() {
+  await database.query("drop schema public cascade; create schema public;");
+}
+
+test("POST to /api/v1/migrations should return 201", async () => {
+  const response = await fetch("http://localhost:3000/api/v1/migrations", {
+    method: "POST",
+  });
+  expect(response.status).toBe(201);
+
+  const responseBody = await response.json();
+
+  expect(Array.isArray(responseBody)).toBe(true);
+  expect(responseBody.length).toBeGreaterThan(0);
+});
+
+test("A second POST to /api/v1/migrations should return an empty array", async () => {
+  const response = await fetch("http://localhost:3000/api/v1/migrations", {
+    method: "POST",
+  });
+  expect(response.status).toBe(200);
+
+  const responseBody = await response.json();
+
+  expect(Array.isArray(responseBody)).toBe(true);
+  expect(responseBody.length).toBe(0);
+});
